2c812936d15b19bb76ddcb5b536d78eb869cbd16
lucasamacedo <lucas_am_@hotmail.com>
2021-05-14 18:02:57 -0300

    Coletando dados dos formulários e imprimindo

--- formulariocadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ formulariocadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDados] = useState({});
+  
+  useEffect(() => {
+    console.log(dadosColetados);
+  })
 
   const formulario = [
-    <DadosUsuario aoEnviar={proximo} />,
-    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
-    <DadosEntrega aoEnviar={aoEnviar} />,
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={coletarDados} />,
   ];
+
+  function coletarDados(dados) {
+    setDados({...dadosColetados, ...dados});
+    console.log(dadosColetados);
+    proximo();
+  }
 
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
