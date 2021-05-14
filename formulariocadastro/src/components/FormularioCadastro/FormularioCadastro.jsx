4378877f32fc91dc789f76bd646ecbe675185fa3
lucasamacedo <lucas_am_@hotmail.com>
2021-05-14 14:09:18 -0300

    Resolvendo problema de assincronicidade

--- formulariocadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ formulariocadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,26 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
   const [nome, setNome] = useState("Lucas");
+  const [sobrenome, setSobrenome] = useState("");
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        console.log(nome);
+        console.log(nome, sobrenome);
       }}
     >
       <TextField
-      value={nome}
         id="nome"
         label="Nome"
         variant="outlined"
         margin="normal"
+        value={nome}
         onChange={(event) => {
-          setNome(event.target.value);
-          if(nome.length >= 3) {
-            setNome(nome.substr(0, 3));
+          let tmpNome = event.target.value;
+          if (tmpNome.length >= 3) {
+            tmpNome = tmpNome.substr(0, 3);
           }
+          setNome(tmpNome);
         }}
         fullWidth
       />
@@ -29,6 +32,10 @@ function FormularioCadastro() {
         label="Sobrenome"
         variant="outlined"
         margin="normal"
+        value={sobrenome}
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
         fullWidth
       />
       <TextField
