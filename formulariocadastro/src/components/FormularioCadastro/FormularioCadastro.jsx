8f076b27b32993e4a898b390accab72bac3df20e
lucasamacedo <lucas_am_@hotmail.com>
2021-05-14 17:41:58 -0300

    Refatorando o código retirando estrutura switch

--- formulariocadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ formulariocadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,4 +1,3 @@
-import { Typography } from "@material-ui/core";
 import React, { useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
@@ -7,24 +6,16 @@ import DadosUsuario from "./DadosUsuario";
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
 
+  const formulario = [
+    <DadosUsuario aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={aoEnviar} />,
+  ];
+
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
-
-  function formularioAtual(etapa) {
-    switch (etapa) {
-      case 0:
-        return <DadosUsuario aoEnviar={proximo} />;
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />;
-      case 2:
-        return <DadosEntrega aoEnviar={aoEnviar} />;
-      default:
-        <Typography>Erro ao selecionar formulário.</Typography>;
-    }
-  }
-
-  return <>{formularioAtual(etapaAtual)}</>;
+  return <>{formulario[etapaAtual]}</>;
 }
 
 export default FormularioCadastro;
